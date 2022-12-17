6babed5a9855ef524cf07376a629c4cc64a7593f
AndyTkach <andrutka4@gmai.com>
2022-12-17 21:00:46 +0300

    Сломался на четвертой :(

---
+++ Ex8/Program.cs
@@ -0,0 +1,21 @@
+﻿//ЗАДАЧА 8 
+Console.WriteLine("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int number = int.Parse(userInput);
+int i = 1;
+
+while (i = 1, i <= number, i++)
+{
+    if (i%2 == 0)
+    {
+        Console.WriteLine ($"{i} + " ", ");
+    }
+    else
+    {
+        // Я не понимаю
+    }
+    i = i + 1;
+}
+
+// ПОМОГИТЕ :((((
