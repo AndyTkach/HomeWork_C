b6774cbc4e591c5ec4843a51b6e5b9dea3fdddb4
AndyTkach <andrutka4@gmai.com>
2022-12-17 20:14:20 +0300

    Сделал третье задание

---
+++ Ex6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int number = int.Parse(userInput);
+
+if (number%2 == 0)
+{
+    Console.WriteLine ($"{number} -> да");
+}
+else 
+{
+    Console.WriteLine ($"{number} -> нет");
+}
