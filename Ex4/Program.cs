749afcfb61e7007294a792c8f6bb354c1c02b74b
AndyTkach <andrutka4@gmai.com>
2022-12-17 19:59:39 +0300

    Сделал второе задание

---
+++ Ex4/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine ("Введите первое целое число: ");
+string userInput1 = Console.ReadLine () ?? "";
+Console.WriteLine ("Введите второе целое число: ");
+string userInput2 = Console.ReadLine () ?? "";
+Console.WriteLine ("Введите третье целое число: ");
+string userInput3 = Console.ReadLine () ?? "";
+
+Console.WriteLine ("Спасибо!");
+
+int number1 = int.Parse(userInput1);
+int number2 = int.Parse(userInput2);
+int number3 = int.Parse(userInput3);
+
+int max = 0;
+int finMax = 0;
+
+if (number1 > number2)
+{
+    max = number1;
+        if (max > number3)
+            {max = finMax;
+            Console.WriteLine($"{number1},{number2},{number3} -> {finMax}");
+            }
+        else
+        {
+            finMax = number3;
+            Console.WriteLine($"{number1},{number2},{number3} -> {finMax}");
+        }
+
+}
+else
+{
+    max = number2;
+    if (max > number3)
+    {
+        max = finMax;
+        Console.WriteLine($"{number1},{number2},{number3} -> {finMax}");
+    }
+    else
+    {
+        finMax = number3;
+        Console.WriteLine($"{number1},{number2},{number3} -> {finMax}");  
+    }
+}
